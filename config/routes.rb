b372356f2138e765cf3506a763acb362bd600378
Yaakov Selkowitz <yselkowi@redhat.com>
2021-05-05 02:31:41 -0400

    Upgrade to Rails 6.1

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
