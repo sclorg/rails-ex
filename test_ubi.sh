0f2d2867e1cc9c4303a32a0dec13efb10ac13d26
Pavel Valena <pvalena@redhat.com>
2021-05-05 17:04:08 +0200

    Add test lock file an generation scripts.

--- test_ubi.sh
+++ test_ubi.sh
@@ -5,7 +5,7 @@ export http_proxy=
 tst="$(readlink -e "$(dirname "$0")/test_cont.sh")"
 [[ -x "$tst" ]] || exit 1
 
-for x in 2{5..7} 30; do
+for x in 2{5..7} 3{0..1}; do
   $tst "registry.access.redhat.com/ubi8/ruby-${x}" \
     || break
 done
