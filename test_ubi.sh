f97db065c12fc06b6846472fdc716917d675614f
Pavel Valena <pvalena@redhat.com>
2021-05-05 17:04:08 +0200

    Add test lock file an generation scripts.

---
+++ test_ubi.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+tst="$(readlink -e "$(dirname "$0")/test_cont.sh")"
+[[ -x "$tst" ]] || exit 1
+
+for x in 2{5..7}; do
+  $tst "registry.access.redhat.com/ubi8/ruby-${x}" \
+    || break
+done
