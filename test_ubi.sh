f55974416906bd68d845fd309209f2f5bd39a8ae
Pavel Valena <pvalena@redhat.com>
2023-08-31 17:46:47 +0200

    test_ubi.sh: do not test with Ruby 2.7

--- test_ubi.sh
+++ test_ubi.sh
@@ -15,7 +15,7 @@ export http_proxy=
 tst="$(readlink -e "$(dirname "$0")/test_cont.sh")"
 [[ -x "$tst" ]] || exit 1
 
-for x in 2{5..7} 3{0..1}; do
+for x in 25 27 3{0..1}; do
   $tst "registry.access.redhat.com/ubi8/ruby-${x}" \
     || break
 done
